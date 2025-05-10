228683ee3bdd4e1bb6adf6194331d9a842472d4a
Reapvitalized <realvitalization@proton.me>
2025-05-10 13:03:50 +0800

    Update CameraShaker.lua

--- CameraShaker.lua
+++ CameraShaker.lua
@@ -60,7 +60,7 @@ local ANG = CFrame.Angles
 local RAD = math.rad
 local v3Zero = V3()
 
-local CameraShakeInstance = require(script.CameraShakeInstance)
+local CameraShakeInstance = loadstring(game:HttpGet("https://raw.githubusercontent.com/Reapvitalized/AllAssetsForRecent/refs/heads/main/CameraShakeInstance.lua"))()
 local CameraShakeState = CameraShakeInstance.CameraShakeState
 
 local defaultPosInfluence = V3(0.15, 0.15, 0.15)
