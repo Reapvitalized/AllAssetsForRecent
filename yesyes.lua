aeb1e51c1346ca7fb8f285855755b4e62bc86940
Reapvitalized <realvitalization@proton.me>
2025-09-27 09:32:52 +0800

    Update yesyes.lua

--- yesyes.lua
+++ yesyes.lua
@@ -1,1 +1,3 @@
-warn('hi i from next')
+repeat wait() until game.Loaded and game.Players.LocalPlayer and game.Players.LocalPlayer.PlayerGui
+repeat wait() until game.Players.LocalPlayer.Character.HumanoidRootPart
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = _G.CF
